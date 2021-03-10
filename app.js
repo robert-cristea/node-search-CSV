894dd6b073d15eaab2f53a714fad6bd0afc7886e
adventure-star <Boristolstyakov@protonmail.com>
2021-03-10 21:06:04 +0700

    firstComplete

---
+++ app.js
@@ -0,0 +1,64 @@
+const fs = require("fs");
+var argv = require("optimist").argv;
+
+
+var recur = function (id,pId){
+    if(pId != ""){
+        id=pId;
+        fs.readFileSync("./company_relations.csv","utf8")
+        .split("\n")
+        .slice(1)
+        .forEach((line) => {
+            const [companyId,name,parentId] = line.split(",");
+            
+            if(companyId == id) { recur(companyId,parentId); if(parentId != "") console.log("-"+id); else console.log(id);} 
+        });
+      }
+      else{
+          // console.log(id+"root");
+     
+          return id;
+      }
+    }
+
+
+fs.readFileSync("./company_relations.csv", "utf8")
+  .split("\n")
+  .slice(1) // header row
+  .forEach((line,index) => {
+    const [id, name, parentId] = line.split(",");
+    
+    
+  });
+
+fs.readFileSync("./land_ownership.csv", "utf8")
+  .split("\n")
+  .slice(1) // header row
+  .forEach((line) => {
+    const [landId, companyId] = line.split(",");
+    if(landId == argv.searchLandId) {
+        
+        fs.readFileSync("./company_relations.csv", "utf8")
+            .split("\n")
+            .slice(1) // header row
+            .forEach((line,index) => {
+                const [id, name, parentId] = line.split(",");
+                if(id == companyId) {
+                    recur(id,parentId);
+                    if(parentId != ""){
+                      console.log("-"+id);
+                    }
+                    else{
+                      console.log(id);
+                    }
+                }
+  });
+
+        
+    }
+  });
+
+  
+
+
+
